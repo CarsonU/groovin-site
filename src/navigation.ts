2023a683836dc45c61fa191082c7c02440866025
carson <carson.umberger@gmail.com>
2026-01-05 16:22:40 -0500

    add facebook link

--- src/navigation.ts
+++ src/navigation.ts
@@ -7,6 +7,7 @@ export const headerData = {
   ],
   actions: [
     { text: 'Instagram', href: 'https://www.instagram.com/groovin_embers/', icon: 'tabler:brand-instagram', ariaLabel: 'Follow us on Instagram' },
+    { text: 'Facebook', href: 'https://www.facebook.com/groovin.embers/', icon: 'tabler:brand-facebook', ariaLabel: 'Follow us on Facebook' },
     { text: 'GigSalad', href: 'https://www.gigsalad.com/the_groovin_embers_hammonton', icon: 'tabler:salad', ariaLabel: 'Book us on GigSalad' }
   ],
 };
