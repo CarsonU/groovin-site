fe371e3c53941b7bc01863b5ab8bc309cf2ff6a4
carson <carson.umberger@gmail.com>
2026-01-05 16:14:44 -0500

    add social media to header

--- src/navigation.ts
+++ src/navigation.ts
@@ -5,5 +5,8 @@ export const headerData = {
     { text: 'About', href: '/about' },
     { text: 'Contact', href: '/contact' },
   ],
-  actions: [{ text: 'Call Us', href: 'tel:+123456789', variant: 'primary' }],
+  actions: [
+    { text: 'Instagram', href: 'https://www.instagram.com/groovin_embers/', icon: 'tabler:brand-instagram', ariaLabel: 'Follow us on Instagram' },
+    { text: 'GigSalad', href: 'https://www.gigsalad.com/the_groovin_embers_hammonton', icon: 'tabler:salad', ariaLabel: 'Book us on GigSalad' }
+  ],
 };
